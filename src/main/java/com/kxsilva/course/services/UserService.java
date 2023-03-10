2b53da0dad0959bacaa6f85a80df810cdcb8ff0b
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-03-10 19:18:21 -0300

    Service layer, component registration, database corretion.

---
+++ src/main/java/com/kxsilva/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.kxsilva.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kxsilva.course.entities.User;
+import com.kxsilva.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
